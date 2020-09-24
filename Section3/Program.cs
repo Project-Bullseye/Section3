f74fc09dc34480a14e1c00da2eda00a5b47f3f4a
Dana Wilson <danalwil@outlook.com>
2020-09-24 11:18:34 -0400

    Completed Section 3 (needs a part from another Section)

--- Section3/Program.cs
+++ Section3/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 
 namespace Section3
 {
     class Program
     {
+        static public string documentPath = "myData.txt";
         static void Main(string[] args)
         {
 
@@ -18,7 +20,12 @@ namespace Section3
                 //This might be where the rest of the code is
 
 
-                Console.WriteLine("Do you want to go again?");
+                using (StreamReader sr = File.OpenText(documentPath))
+                {
+                    StreamWriter sw = new StreamWriter(documentPath);
+                    sw.WriteLine(score);
+                }
+                    Console.WriteLine("Do you want to go again?");
                 string loop = Console.ReadLine();
                 if (loop == "y")
                 {
