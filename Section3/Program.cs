a9626ca74d2440bbde6e3ed267fcc4e2f57e6889
Dana Wilson <danalwil@outlook.com>
2020-09-24 11:23:11 -0400

    Added comments

--- Section3/Program.cs
+++ Section3/Program.cs
@@ -5,7 +5,7 @@ namespace Section3
 {
     class Program
     {
-        static public string documentPath = "myData.txt";
+        static public string documentPath = "myData.txt"; //need a more complete link for the myData.txt
         static void Main(string[] args)
         {
 
@@ -23,7 +23,7 @@ namespace Section3
                 using (StreamReader sr = File.OpenText(documentPath))
                 {
                     StreamWriter sw = new StreamWriter(documentPath);
-                    sw.WriteLine(score);
+                    sw.WriteLine(score); // When other sections are being made, this score is for the myData.txt   AKA the key for keeping track of all score given.
                 }
                     Console.WriteLine("Do you want to go again?");
                 string loop = Console.ReadLine();
