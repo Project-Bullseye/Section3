a772737aef34a2067d408569b62522da65a56d24
Dana Wilson <danalwil@outlook.com>
2020-09-24 10:38:27 -0400

    Add project files.

---
+++ Section3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Section3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+
+
+
+
+
+
+
+                Console.WriteLine("Do you want to go again?");
+                string loop = Console.ReadLine();
+                if (loop == "y")
+                {
+                    playAgain = true;
+                }
+                else if (loop == "n")
+                {
+                    playAgain = false;
+                }
+                else
+                {
+
+                }
+            }
+
+
+        }
+    }
+}
